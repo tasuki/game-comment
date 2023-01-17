7810b277c7d410394a2f48eb32a22358a5e7515c
Vit Brunner <vit.brunner@gmail.com>
2023-01-17 16:06:20 +0100

    Proxy games from littlegolem

--- backend/app/Main.hs
+++ backend/app/Main.hs
@@ -2,11 +2,25 @@
 
 module Main (main) where
 
+import Control.Monad.IO.Class (liftIO)
+import qualified Data.ByteString.Lazy.Char8 as L8
+import Network.HTTP.Client
+import Network.HTTP.Client.TLS (tlsManagerSettings)
 import Web.Scotty
 
+fetchGameRecord :: String -> IO L8.ByteString
+fetchGameRecord gameId = do
+    let gameUrl = "https://www.littlegolem.net/servlet/sgf/" <> gameId <> "/game.sgf"
+    manager <- newManager tlsManagerSettings
+    request <- parseRequest gameUrl
+    response <- httpLbs request manager
+    return $ responseBody response
+
+main :: IO ()
 main = do
-    putStrLn "Starting Server..."
     scotty 6483 $ do
         get "/game/:gameId" $ do
             gameId <- param "gameId"
-            text $ "hello " <> gameId <> " world"
+            response <- liftIO $ fetchGameRecord gameId
+            setHeader "Content-Type" "application/sgf; charset=iso-8859-1"
+            raw $ response
