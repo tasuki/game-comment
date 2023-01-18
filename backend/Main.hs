cb543660be8a7cb2df411eefe57659f181058652
Vit Brunner <vit.brunner@gmail.com>
2023-01-18 11:59:45 +0100

    CORS header and proxy status

--- backend/Main.hs
+++ backend/Main.hs
@@ -6,21 +6,24 @@ import Control.Monad.IO.Class (liftIO)
 import qualified Data.ByteString.Lazy.Char8 as L8
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS (tlsManagerSettings)
+import Network.HTTP.Types.Status (Status)
 import Web.Scotty
 
-fetchGameRecord :: String -> IO L8.ByteString
+fetchGameRecord :: String -> IO (Status, L8.ByteString)
 fetchGameRecord gameId = do
     let gameUrl = "https://www.littlegolem.net/servlet/sgf/" <> gameId <> "/game.sgf"
     manager <- newManager tlsManagerSettings
     request <- parseRequest gameUrl
     response <- httpLbs request manager
-    return $ responseBody response
+    return $ (responseStatus response, responseBody response)
 
 main :: IO ()
 main = do
     scotty 6483 $ do
         get "/game/:gameId" $ do
             gameId <- param "gameId"
-            response <- liftIO $ fetchGameRecord gameId
+            (responseStatus, response) <- liftIO $ fetchGameRecord gameId
+            status responseStatus -- we don't mind too bad if this is off
             setHeader "Content-Type" "application/sgf; charset=iso-8859-1"
+            setHeader "Access-Control-Allow-Origin" "*" -- TODO!
             raw $ response
