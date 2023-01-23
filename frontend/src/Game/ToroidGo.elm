bff199863852dae0ff5af3e63ef337e6067f17e2
Vit Brunner <vit.brunner@gmail.com>
2023-01-23 17:27:50 +0100

    Stone-taking and suicide-prevention in torus

--- frontend/src/Game/ToroidGo.elm
+++ frontend/src/Game/ToroidGo.elm
@@ -4,6 +4,7 @@ import Array exposing (Array)
 import GameHelpers as GH
 import GameRecord as G
 import Html as H
+import List.Extra
 import Replay as R
 import Svg exposing (Svg)
 import Svg.Attributes as SA
@@ -28,37 +29,155 @@ type alias Position =
     }
 
 
+emptyPosition : Int -> Position
 emptyPosition size =
     { size = size
     , stones = Array.initialize (size * size) (always Nothing)
     }
 
 
-getStone : Int -> Int -> Int -> Stones -> Stone
-getStone size x y stones =
-    Array.get ((x - 1) + (y - 1) * size) stones |> Maybe.withDefault Nothing
+getStone : G.Coords -> Position -> Stone
+getStone coords position =
+    Array.get ((coords.x - 1) + (coords.y - 1) * position.size) position.stones
+        |> Maybe.withDefault Nothing
 
 
-setStone : Int -> Int -> Int -> Stone -> Stones -> Stones
-setStone size x y stone stones =
-    Array.set ((x - 1) + (y - 1) * size) stone stones
+setStone : Stone -> G.Coords -> Position -> Position
+setStone stone coords position =
+    { position
+        | stones =
+            Array.set
+                ((coords.x - 1) + (coords.y - 1) * position.size)
+                stone
+                position.stones
+    }
+
+
+allCoords : Position -> List G.Coords
+allCoords position =
+    let
+        toSize =
+            List.range 0 (position.size - 1)
+    in
+    List.concatMap (\x -> List.map (\y -> { x = x, y = y }) toSize) toSize
+
+
+positionToStones : Position -> List ( G.Coords, Stone )
+positionToStones position =
+    List.map (\c -> ( c, getStone c position )) (allCoords position)
+
+
+findGroupWithoutLiberties : Position -> G.Player -> G.Coords -> List G.Coords
+findGroupWithoutLiberties position player coords =
+    -- returns list of coords of the libertyless group, empty list if has liberties
+    let
+        empty =
+            emptyPosition position.size
+
+        coordsFromPosition : Position -> List G.Coords
+        coordsFromPosition explored =
+            positionToStones explored
+                |> List.filter (\( _, s ) -> s == Just player)
+                |> List.map (\( c, _ ) -> c)
+
+        hasEmptyNeighbors : G.Coords -> Bool
+        hasEmptyNeighbors toExplore =
+            neighbors toExplore position
+                |> List.any (\c -> getStone c position == Nothing)
+
+        ownNeighbors : G.Coords -> List G.Coords
+        ownNeighbors toExplore =
+            neighbors toExplore position
+                |> List.filter (\c -> getStone c position == Just player)
+
+        isClosed : Position -> G.Coords -> Bool
+        isClosed closed toExplore =
+            case getStone toExplore closed of
+                Just _ ->
+                    True
+
+                Nothing ->
+                    False
+
+        newOpenClosed : G.Coords -> List G.Coords -> Position -> ( List G.Coords, Position )
+        newOpenClosed toExplore open closed =
+            let
+                toOpen =
+                    ownNeighbors toExplore
+                        |> List.Extra.filterNot (isClosed closed)
+
+                newOpen =
+                    toOpen ++ open
+
+                newClosed =
+                    List.foldl (\c -> setStone (Just player) c) closed toOpen
+            in
+            ( newOpen, newClosed )
+
+        findLibertyless : ( List G.Coords, Position ) -> Position
+        findLibertyless ( open, closed ) =
+            case open of
+                [] ->
+                    -- libertyless
+                    closed
+
+                toExplore :: exploreLater ->
+                    if hasEmptyNeighbors toExplore then
+                        -- has at least one liberty
+                        empty
+
+                    else
+                        -- explore further
+                        findLibertyless <| newOpenClosed toExplore exploreLater closed
+    in
+    findLibertyless ( [ coords ], setStone (Just player) coords empty )
+        |> coordsFromPosition
+
+
+neighbors : G.Coords -> Position -> List G.Coords
+neighbors coords pos =
+    List.map (normaliseCoords pos)
+        [ { coords | x = coords.x - 1 }
+        , { coords | x = coords.x + 1 }
+        , { coords | y = coords.y - 1 }
+        , { coords | y = coords.y + 1 }
+        ]
+
+
+takeAll : G.Player -> G.Coords -> Position -> Position
+takeAll player coords pos =
+    neighbors coords pos
+        |> List.filter (\c -> getStone c pos == (Just <| G.otherPlayer player))
+        |> List.concatMap (findGroupWithoutLiberties pos (G.otherPlayer player))
+        |> List.foldl (\c -> setStone Nothing c) pos
+
+
+maybePlay : G.Player -> G.Coords -> Position -> Maybe Position
+maybePlay player coords position =
+    let
+        positionAfterMove =
+            setStone (Just player) coords position
+
+        positionAfterTake =
+            takeAll player coords positionAfterMove
+    in
+    if findGroupWithoutLiberties positionAfterTake player coords == [] then
+        Just positionAfterTake
+
+    else
+        Nothing
 
 
 add : G.Move -> Position -> Position
 add { player, play } position =
     case play of
         G.Place coords ->
-            let
-                -- TODO uh there might be some limitations, also stone-taking
-                stones =
-                    setStone
-                        position.size
-                        coords.x
-                        coords.y
-                        (Just player)
-                        position.stones
-            in
-            { position | stones = stones }
+            case maybePlay player coords position of
+                Just newPos ->
+                    newPos
+
+                Nothing ->
+                    position
 
         _ ->
             position
@@ -67,6 +186,23 @@ add { player, play } position =
 positionFromMoves : Int -> List G.Move -> Position
 positionFromMoves boardSize moves =
     List.foldl add (emptyPosition boardSize) moves
+
+
+normaliseCoords : Position -> G.Coords -> G.Coords
+normaliseCoords position coords =
+    let
+        normaliseCoord : Int -> Int
+        normaliseCoord coord =
+            if coord < 1 then
+                coord + position.size
+
+            else if coord > position.size then
+                coord - position.size
+
+            else
+                coord
+    in
+    { x = normaliseCoord coords.x, y = normaliseCoord coords.y }
 
 
 
@@ -123,32 +259,18 @@ drawLines min max =
     horizontal ++ vertical
 
 
-drawStones : Int -> Int -> Position -> Maybe G.Move -> (G.Coords -> msg) -> List (Svg msg)
-drawStones min max position lastMove playMsg =
+drawStones : Int -> Int -> Position -> Maybe G.Move -> G.Player -> (G.Coords -> msg) -> List (Svg msg)
+drawStones min max position lastMove onMove playMsg =
     let
-        normaliseCoord : Int -> Int
-        normaliseCoord coord =
-            if coord < 1 then
-                coord + position.size
-
-            else if coord > position.size then
-                coord - position.size
-
-            else
-                coord
-
         maybeShowStone : G.Coords -> Svg msg
         maybeShowStone coords =
             let
-                ( normX, normY ) =
-                    ( normaliseCoord coords.x, normaliseCoord coords.y )
-
                 normCoords =
-                    { x = normX, y = normY }
+                    normaliseCoords position coords
 
                 stone : Stone
                 stone =
-                    getStone position.size normX normY position.stones
+                    getStone normCoords position
 
                 class : List (H.Attribute msg)
                 class =
@@ -170,14 +292,19 @@ drawStones min max position lastMove playMsg =
                         []
 
                 Nothing ->
-                    Svg.circle
-                        [ SA.cx <| String.fromInt coords.x
-                        , SA.cy <| String.fromInt coords.y
-                        , SA.r "0.45"
-                        , SA.fill "transparent"
-                        , SE.onClick <| playMsg normCoords
-                        ]
-                        []
+                    case maybePlay onMove coords position of
+                        Just _ ->
+                            Svg.circle
+                                [ SA.cx <| String.fromInt coords.x
+                                , SA.cy <| String.fromInt coords.y
+                                , SA.r "0.45"
+                                , SA.fill "transparent"
+                                , SE.onClick <| playMsg normCoords
+                                ]
+                                []
+
+                        Nothing ->
+                            Svg.svg [] []
     in
     List.map maybeShowStone (GH.coordList min max)
 
@@ -203,5 +330,6 @@ view replay playMsg =
                 max
                 position
                 (R.lastMove replay)
+                (G.onMove replay.lookingAt.move G.ToroidGo)
                 playMsg
         )
