2f706c14225207f84603251f486a1b81c432c61c
Vit Brunner <vit.brunner@gmail.com>
2026-07-27 16:39:12 +0200

    More help

--- frontend/src/Page/Help.elm
+++ frontend/src/Page/Help.elm
@@ -43,13 +43,16 @@ view _ =
     , extraClass = "narrower"
     , content =
         [ H.p [] [ H.text "The UI is a work in progress!" ]
+        , list
+            [ H.text "The hamburger menu in the top left collapses/expands the top bar so it doesn't take too much space on mobile"
+            ]
         , H.p [] [ H.text "Navigating the game:" ]
         , list
             [ H.text "Use the arrows on the sidebar to move forward/backward in the game."
             , H.text "Click anywhere on the board to start a variation."
             , H.text "Click on a highlighted move in a comment to preview it."
             ]
-        , H.p [] [ H.text "Keyboard shortcuts:" ]
+        , H.p [] [ H.text "Keyboard shortcuts for viewing:" ]
         , list
             [ H.text "Use the left/right (or h/l) keys to see next/previous move."
             , H.text "Up/down to switch between the analysis mode and go through the comments."
@@ -59,6 +62,13 @@ view _ =
             [ H.text "g/G to go to the start/end of the game"
             , H.text "k/j for previous/next variation"
             , H.text "x to delete the current variation"
+            ]
+        , H.p [] [ H.text "Commenting" ]
+        , list
+            [ H.text "You can use the 'Add Current Variation' button to add a variation"
+            , H.text "|10.a1 means to start a new variation at move 10, playing at a1"
+            , H.text "10.a1 means to backtrack to the most recent move 9 (whether in the comment or game record) and continues with a1"
+            , H.text "a1 highlights the a1 point at the current board position"
             ]
         ]
     , sidebar = Page.sideHelp
